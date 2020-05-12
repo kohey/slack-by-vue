c5d911658c6e4b8de6e8be833f565d1a873c6380
kohey <kawasonblog@gmail.com>
2020-05-12 14:50:33 +0900

    Messages を Message に分割

--- components/Messages.vue
+++ components/Messages.vue
@@ -1,24 +1,22 @@
 <template>
-  <div class="chats-container">
-      <!-- 1つのチャット -->
-      <div class="chat-container">
-        <div class="thumbnail-container">
-          <img src="https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg" />
-        </div>
-        <div class="message-container">
-          <div class="user-name">cohki0305</div>
-          <div class="message">今日も良い天気ですね</div>
-        </div>
-      </div>
-      <!-- 1つのチャット -->
-      <div class="chat-container">
-        <div class="thumbnail-container">
-          <img src="https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg" />
-        </div>
-        <div class="message-container">
-          <div class="user-name">cohki0305</div>
-          <div class="message">今日も良い天気ですね</div>
-        </div>
-      </div>
-    </div>
+ <div class="chats-container">
+   <message />
+   <message />
+ </div>
 </template>
+
+<script>
+import Message from '~/components/Message'
+
+export default {
+ components: {
+   Message
+ }
+}
+</script>
+
+<style>
+.chats-container {
+ padding: 16px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -13,29 +13,3 @@ export default {
   }
 }
 </script>
-
-<style>
-/* ここから追加 */
-.chats-container {
- padding: 16px;
-}
-
-.chat-container {
- display: flex;
- padding: 8px;
-}
-
-.thumbnail-container {
- margin-right: 16px;
-}
-
-.thumbnail-container img {
- width: 50px;
- height: 50px;
- border-radius: 10px;
-}
-
-.user-name {
- font-weight: bold;
-}
-</style>
