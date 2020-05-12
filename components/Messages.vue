fa85ee456d5eb86ad328f56f66569489be9a1a5e
kohey <kawasonblog@gmail.com>
2020-05-12 14:35:23 +0900

    Messages コンポーネントを作成

---
+++ components/Messages.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="chats-container">
+      <!-- 1つのチャット -->
+      <div class="chat-container">
+        <div class="thumbnail-container">
+          <img src="https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg" />
+        </div>
+        <div class="message-container">
+          <div class="user-name">cohki0305</div>
+          <div class="message">今日も良い天気ですね</div>
+        </div>
+      </div>
+      <!-- 1つのチャット -->
+      <div class="chat-container">
+        <div class="thumbnail-container">
+          <img src="https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg" />
+        </div>
+        <div class="message-container">
+          <div class="user-name">cohki0305</div>
+          <div class="message">今日も良い天気ですね</div>
+        </div>
+      </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,20 @@
 <template>
   <div class="container">
-    <div class="chats-container">
-      <!-- 1つのチャット -->
-      <div class="chat-container">
-        <div class="thumbnail-container">
-          <img src="https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg" />
-        </div>
-        <div class="message-container">
-          <div class="user-name">cohki0305</div>
-          <div class="message">今日も良い天気ですね</div>
-        </div>
-      </div>
-      <!-- 1つのチャット -->
-      <div class="chat-container">
-        <div class="thumbnail-container">
-          <img src="https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg" />
-        </div>
-        <div class="message-container">
-          <div class="user-name">cohki0305</div>
-          <div class="message">今日も良い天気ですね</div>
-        </div>
-      </div>
-    </div>
+    <messages />
   </div>
 </template>
 
 <script>
+import Messages from '~/components/Messages.vue'
+
 export default {
   components: {
-
+    Messages
   }
 }
 </script>
 
-<style scoped>
+<style>
 /* ここから追加 */
 .chats-container {
  padding: 16px;
