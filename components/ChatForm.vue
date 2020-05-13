17324a3bfaa2370af2c30d45bd3583b7fa8a86e9
kohey <kawasonblog@gmail.com>
2020-05-13 14:37:37 +0900

    ElementUI で Dialog を表示

--- components/ChatForm.vue
+++ components/ChatForm.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="input-container">
     <textarea v-model="text" v-on:keydown.enter="addMessage"></textarea>
+    <el-dialog
+      title="Tips"
+      :visible="dialogVisible"
+      width="30%">
+    <span>This is a message</span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import { db } from '~/plugins/firebase.js'
+import Vue from "vue";
+import ElementUI from "element-ui";
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
 export default {
-  methods:{
-    data(){
+  data(){
       return {
+        dialogVisible: true,
         text: null
       }
     },
+  methods:{
     addMessage(event){
       if(this.keyDownedForJPConversion(event)) { return }
       const channelId = this.$route.params.id
@@ -24,7 +36,8 @@ export default {
     keyDownedForJPConversion(event){
       const codeForConversion = 229
       return codeForConversion === event.keyCode
-    }
+    },
+
   }
 }
 </script>
