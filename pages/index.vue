d72b3c14c09fc378a8a37c7f80f82f1129398b9e
kohey <kawasonblog@gmail.com>
2020-05-13 14:12:30 +0900

    ログイン処理を実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,25 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <button @click="login">ログイン</button>
+  </div>
 </template>
+
+<script>
+import {firebase } from "~/plugins/firebase.js";
+export default {
+
+  methods:{
+    login(){
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithPopup(provider)
+        .then((result)=>{
+          const user = result.user
+          console.log(user)
+        })
+        .catch((error)=>{
+          window.alert(error)
+        })
+    }
+  }
+}
+</script>
